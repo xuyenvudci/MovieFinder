dd0a49a0073c95fc39c820970dae552a7ca840ec
Xuyen Vu <thi.vu@dci-student.org>
2025-08-19 21:26:04 +0200

    Update SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useMyContext } from '../context/AppContext';
 import type { Movie } from '../libs/type';
-import styles from '../pages/home.module.css'
 
 type Status = "idle" | "loading" | "done";
 
 export default function SearchBar() {
-  const [search, setSearch] = useState('');
+  const [searchMovie, setSearchMovie] = useState('');
+  const [searchType, setSearchType] = useState('');
+  const [searchYear, setSearchYear] = useState('');
   const [movies, setMovies] = useState<Movie[]>([]);
   const [status, setStatus] = useState<Status>("idle");
   const context = useMyContext();
@@ -15,10 +16,11 @@ export default function SearchBar() {
   if (!context) return <p>Context is unavailable</p>;
   const { likedMovies, addToFavourite, removeFromFavourite } = context;
 
-  const fetchMovies = async (query: string) => {
-    if (!query) return;
+  const fetchMovies = async (query1: string, query2:string, query3: string) => {
+    if (!query1) return;
     setStatus("loading");
-    const res = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=16c8cb0e`);
+
+    const res = await fetch(`https://www.omdbapi.com/?s=${query1}&type=${query2}&y=${query3}&apikey=16c8cb0e`);
     const data = await res.json();
     if (data.Search) {
       setMovies(data.Search);
@@ -30,49 +32,51 @@ export default function SearchBar() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetchMovies(search);
+    fetchMovies(searchMovie, searchType, searchYear);
   };
 
-return (
-  <div>
-    <form onSubmit={handleSubmit} className={styles.searchForm}>
-      <input
-        type="text"
-        name="searchMovie"
-        placeholder="Search by movie name"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button type="submit">Find</button>
-       <button type="button" onClick={() => { setSearch(''); setMovies([]); setStatus("idle"); }}>
-    Clear
-  </button>
-    </form>
-
-    {status === "idle" && null}
-    {status === "loading" && <p>Loading...</p>}
-    {status === "done" && movies.length === 0 && <p>Nothing found</p>}
-    
-    {status === "done" && movies.length > 0 && (
-      <div className={styles.movieGrid}>
-        {movies.map((movie) => {
-          const isLike = likedMovies.some(
-            (m: Movie) => m.imdbID === movie.imdbID
-          );
-
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchMovie"
+          placeholder="Search by movie name"
+          value={searchMovie}
+          onChange={(e) => setSearchMovie(e.target.value)}
+        />
+        <input type="text"
+          name="searchYear"
+          placeholder="Search by year"
+          value={searchYear}
+          onChange={(e) => setSearchYear(e.target.value)}
+        />
+        <select
+          name="searchType"
+          onChange={(e) => setSearchType(e.target.value)}
+        >
+          <option value="">All type</option>
+          <option value="movie">Movie</option>
+          <option value="series">Series</option>
+          <option value="episode">Episode</option>
+        </select>
+        <button type="submit">Find</button>
+      </form>
+      
+      {status === "idle" && null}
+      {status === "loading" && <p>Loading...</p>}
+      {status === "done" && movies.length === 0 && <p>Nothing found</p>}
+      {status === "done" && movies.length > 0 && (
+        movies.map((movie) => {
+          const isLike = likedMovies.some((m: Movie) => m.imdbID === movie.imdbID);
           return (
-            <div key={movie.imdbID} className={styles.movieCard}>
-              <NavLink to={`/movies/${movie.imdbID}`}>
-                <img src={movie.Poster} alt={movie.Title} />
-              </NavLink>
-              <div className={styles.movieInfo}>
+            <ul key={movie.imdbID}>
+              <li>
                 <NavLink to={`/movies/${movie.imdbID}`}>
-                  <h3>
-                    {movie.Title} ({movie.Year})
-                  </h3>
+                  {movie.Title} ({movie.Year})
                 </NavLink>
                 <span
-                  className={styles.like}
+                  className="like"
                   onClick={() =>
                     isLike
                       ? removeFromFavourite(movie.imdbID)
@@ -81,12 +85,16 @@ return (
                 >
                   {isLike ? "❤️" : "🤍"}
                 </span>
-              </div>
-            </div>
+              </li>
+              <li>
+                <NavLink to={`/movies/${movie.imdbID}`}>
+                  <img src={movie.Poster} alt={movie.Title} />
+                </NavLink>
+              </li>
+            </ul>
           );
-        })}
-      </div>
-    )}
-  </div>
-);
+        })
+      )}
+    </div>
+  );
 }
